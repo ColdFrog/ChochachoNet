9452fe14641b89e9c3af4257f28005d9dbff9cb8
Chris Daniels <coldfrog@gmail.com>
2015-05-15 01:36:40 -0500

    Added affiliates page and styles

---
+++ ChoLinks.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Language="C#" AutoEventWireup="false" Inherits="Chochachonet.ChoLinks" ValidateRequest="false" EnableSessionState="false"  MasterPageFile="~/page.master"  %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="Main" Runat="Server">
+	<div id="site_affiliates">
+		<div><a href="http://www.shrugguy.com" target="_blank">Shrug Guy ¯\_(ツ)_/¯</a></div>
+	</div>
+</asp:Content>
---
+++ ChoLinks.aspx.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: cdaniels
+ * Date: 5/15/2015
+ * Time: 1:21 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+namespace Chochachonet
+{
+	/// <summary>
+	/// Description of ChoLinks
+	/// </summary>
+	public class ChoLinks : Page
+	{	
+		private void Page_Load(object sender, System.EventArgs e)
+		{
+			if(IsPostBack)
+			{
+			}
+		}
+	}
+}
